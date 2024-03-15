8b50779cb55e9ca32a222282230a96dd78d484cf
koshyar <ensw03420@student.su.edu.krd>
2024-03-15 14:16:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  let dist = block - 42;
+  if (dist < 0) dist *= -1;
+  return dist;
+}
+
+function distanceFromHqInFeet(block) {
+  let dist = (block - 42) * 264;
+  if (dist < 0) dist *= -1;
+  return dist;
+}
+
+function distanceTravelledInFeet(first, second) {
+  let dist = (first - second) * 264;
+  if (dist < 0) dist *= -1;
+  return dist;
+}
+
+function calculatesFarePrice(first, second) {
+  let dist = (first - second) * 264;
+  if (dist < 0) dist *= -1;
+
+  if (dist <= 400) return 0;
+  else if (dist < 2000) return 0.02 * (dist - 400);
+  else if (dist <= 2500) return 25;
+  else return "cannot travel that far";
+}
